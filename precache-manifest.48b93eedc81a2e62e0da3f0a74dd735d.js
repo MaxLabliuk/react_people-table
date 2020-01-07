078159ef202f2cc8e08fc281f22e34109dcb1c16
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-01-08 00:15:44 +0200

    Updates

---
+++ precache-manifest.48b93eedc81a2e62e0da3f0a74dd735d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d002a8fe6c7689e9738e917c13d0973",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "9ee23bf59dc8a78d74d7",
+    "url": "/react_people-table/static/css/main.3df289d5.chunk.css"
+  },
+  {
+    "revision": "e8669e22294010fe4663",
+    "url": "/react_people-table/static/js/2.9df549ec.chunk.js"
+  },
+  {
+    "revision": "9ee23bf59dc8a78d74d7",
+    "url": "/react_people-table/static/js/main.0217d1bc.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
